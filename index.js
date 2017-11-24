7f5ddc0e92a71c0fb3cb531028e9b5c0f8150b48
Beth Schofield <bethmschofield@gmail.com>
2017-11-24 18:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(driverArray) {
-  driverArray.forEach (function{
-    console.log (driver.name)
+  driverArray.forEach (function(driver){
+    console.log(driver.name);
   })
 }
